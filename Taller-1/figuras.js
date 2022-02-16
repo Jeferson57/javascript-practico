325b921f47f7c5f77acec55d66da5d4cf34c7920
Jeferson <alexandermejicanos57@gmail.com>
2022-02-15 22:16:51 -1200

    Calculating geometric figures in JavaScript with fixed values

---
+++ Taller-1/figuras.js
@@ -0,0 +1,61 @@
+// Código del cuadrado
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado mide: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado mide: " + areaCuadrado + "cm^2");
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+console.log(
+    "Los lados del triángulo miden: "
+    + ladoTriangulo1 + "cm, "
+    + ladoTriangulo2 + "cm, "
+    + baseTriangulo + "cm"
+);
+console.log("La altura del triángulo es de: " + alturaTriangulo + "cm")
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo mide: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo mide: " + areaTriangulo + "cm^2");
+
+console.groupEnd();
+
+// Código del circulo
+console.group("Circulo");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del círculo es: " + radioCirculo + "cm");
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2
+console.log("El diámetro del círculo es: " + diametroCirculo + "cm");
+
+
+// PI
+const PI = Math.PI
+console.log("PI es: " + PI);
+
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perímetro del círculo es: " + perimetroCirculo + "cm");
+
+
+// Área
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log("El área del círculo es: " + areaCirculo + "cm^2");
+
+console.groupEnd();
